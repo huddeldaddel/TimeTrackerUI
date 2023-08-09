9ae7c083d2e444ad20244c070205ecaa2989351c
huddeldaddel <35447824+huddeldaddel@users.noreply.github.com>
2023-08-09 21:03:37 +0200

    table-test

--- src/components/log/log-entry.js
+++ src/components/log/log-entry.js
@@ -3,11 +3,22 @@ import { LogEntryApi } from '../../services/log-entry-api';
 
 @inject(LogEntryApi)
 export class LogEntry {
-
+    
+    editMode = false;
     @bindable entry;
 
     constructor(logEntryApi) {
         this.logEntryApi = logEntryApi;        
+    }    
+
+    attached() {        
+        if(!this.entry) {
+            this.editMode = true;
+        }
+    }
+
+    toggleMode() {
+        this.editMode = true;
     }
 
 }
