0f3979abe5458d473469bff545ad76f72f73f103
huddeldaddel <35447824+huddeldaddel@users.noreply.github.com>
2023-08-09 12:52:30 +0200

    Daily log entries can be collapsed

--- src/components/daily-log/daily-log.js
+++ src/components/daily-log/daily-log.js
@@ -5,11 +5,13 @@ export class DailyLog {
     @bindable date;
 
     constructor() {
+        this.collapsed = true;
         this.header = "";
     }
 
-    attached() {
-        this.header = this.formatDate(this.date);
+    attached() {        
+        this.header = this.formatDate(this.date);        
+        this.collapsed = this.header !== this.formatDate(new Date());
     }
 
     formatDate(date = new Date()) {
@@ -17,6 +19,10 @@ export class DailyLog {
         const month = date.toLocaleString('default', { month: '2-digit' });
         const day = date.toLocaleString('default', { day: '2-digit' });
         return [year, month, day].join('-');
+    }
+
+    toggleCollapsed() {
+        this.collapsed = !this.collapsed;
     }
 
 }
