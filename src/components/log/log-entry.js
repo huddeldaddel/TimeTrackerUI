061b5c01222a04afed0ac91572caa0b9b5a6ebfa
huddeldaddel <35447824+huddeldaddel@users.noreply.github.com>
2023-08-10 09:43:18 +0200

    Implemented calculation of duration

--- src/components/log/log-entry.js
+++ src/components/log/log-entry.js
@@ -1,6 +1,51 @@
 import { bindable, inject, observable } from 'aurelia-framework';
 import { LogEntryApi } from '../../services/log-entry-api';
 
+class Duration {
+
+    start = "";
+    end = "";
+
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    getDurationInMinutes() {
+        if(!this.start || !this.end) {
+            return null;
+        }
+
+        let a = new Date();
+        a.setHours(this.start.split(":")[0]);
+        a.setMinutes(this.start.split(":")[1]);
+
+        let b = new Date(a);
+        b.setHours(this.end.split(":")[0]);
+        b.setMinutes(this.end.split(":")[1]);
+
+        if(b < a) {
+            b.setDate(b.getDate() +1);
+        }
+
+        var hours = Math.floor(Math.abs(b - a) / (60 * 60 * 1000));
+        var minutes = (Math.abs(b - a) / (60 * 1000)) % 60;
+        return hours * 60 + minutes;        
+    }
+
+    getDurationAsString() {
+        let total = this.getDurationInMinutes();
+        if(null == total) {
+            return "";
+        }
+
+        let hours = Math.floor(total / 60);
+        let minutes = total % 60;
+        return `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`
+    }        
+    
+}
+
 @inject(LogEntryApi)
 export class LogEntry {
 
@@ -62,9 +107,8 @@ export class LogEntry {
     }
 
     calculateDuration() {
-        if("" !== this.start && "" !== this.end) {
-            console.debug("duration", this.start, this.end);
-        }
+        let duration = new Duration(this.start, this.end);
+        this.duration = duration.getDurationAsString();        
     }
 
 }
